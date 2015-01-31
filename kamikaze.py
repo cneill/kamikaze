69ad2dca2cfa48a8c6eb1ab8a1eea3f2f4c9bb43
Charles Neill <cneill@utexas.edu>
2015-01-31 02:16:22 -0600

    readme

--- kamikaze.py
+++ kamikaze.py
@@ -9,8 +9,8 @@ Options:
     list                     Attack with a wordlist
     brute                    Attack by generating permutations of all characters in the range
         --charset=STRING     A string of characters to use in the attack [default: abcdefghijklmnopqrstuvwxyz]
-        --min_len=INT     Specify the maximum length of a generated word [default: 6]
-        --max_len=INT     Specify the minimum length of a generated word [default: 1]
+        --min_len=INT        Specify the maximum length of a generated word [default: 6]
+        --max_len=INT        Specify the minimum length of a generated word [default: 1]
     path                     Send strings to the URL
     headers                  Send strings to each of the header fields
     --multidir               Go multiple directories deep
